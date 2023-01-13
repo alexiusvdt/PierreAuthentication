2b008c9d79fcdbdd23dc2e456b4fb39c71d788bb
Alex Johnson <alex.johnson293@gmail.com>
2023-01-13 08:45:51 -0800

    establish models & rename production directory

---
+++ Bakery/Models/Flavor.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models
+{
+  public class Flavor
+  {
+    public int FlavorId { get; set; }
+    [Required(ErrorMessage = "This field cannot be blank! Try again.")]
+    public string Name { get; set; }
+    public List<FlavorTreat> JoinEntities { get; }
+  }
+}
