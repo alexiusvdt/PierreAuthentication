0cadd8e745ddd11bdc1deea0b6948f06478e7541
Alex Johnson <alex.johnson293@gmail.com>
2023-01-13 09:45:34 -0800

    add Flavor controller

---
+++ Bakery/Controllers/FlavorsController.cs
@@ -0,0 +1,126 @@
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bakery.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly BakeryContext _db;
+
+    public FlavorsController(BakeryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.PageTitle = "View all flavors";
+      List<Flavor> FlavorList = _db.Flavors.Include(flavor => flavor.JoinEntities)
+          .ThenInclude(join => join.Treat).OrderByDescending(flavor => flavor.JoinEntities.Count).ToList();
+
+      return View(FlavorList);
+    }
+
+    public ActionResult Details(int id)
+    {
+      Flavor thisflavor = _db.Flavors
+          .Include(flavor => flavor.JoinEntities)
+          .ThenInclude(join => join.Treat)
+          .FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.PageTitle = $"{thisFlavor.Name} Details";
+      return View(thisFlavor);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "Add a flavor";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      ViewBag.PageTitle = "Flavors";
+      if (!ModelState.IsValid)
+      {
+        return View(flavor);
+      }
+      else
+      {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult AddTreat(int id)
+    {
+      Flavor thisFlavor = _db.Flavors
+      .Include(flavor => flavor.JoinEntities)
+      .ThenInclude(join => join.Treat)
+      .FirstOrDefault(flavor => flavor.FlavorId == id);
+
+      ViewBag.MachineId = new SelectList(_db.Treats, "TreatId", "Name");
+      ViewBag.PageTitle = $"Add a treat to {thisFlavor.Name}'s profile";
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult AddTreat(Flavor flavor, int treatId)
+    {
+      #nullable enable
+      FlavorTreat? joinEntity = _db.FlavorTreats.FirstOrDefault(join => (join.TreatId == treatId && join.FlavorId == flavor.FlavorId));
+      #nullable disable
+      if (joinEntity == null && machineId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat() { TreatId = treatId, FlavorId = flavor.FlavorId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = flavor.FlavorId });
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavors => flavors.FlavorId == id);
+      ViewBag.PageTitle = $"Modify {thisFlavor.Name}";
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Flavors.Update(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.PageTitle = $"Delete {thisFlavor.Name}";
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      FlavorTreat joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
